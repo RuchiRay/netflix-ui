717a775147ead8aa559a44fd2456006ff8434cc3
RuchiRay <ruchiray18@gmail.com>
2023-11-24 15:56:01 +0530

    added sign in page

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -6,38 +6,41 @@ const SignUp = () => {
     const router = useRouter()
 
   return (
-    <div className="bg-black/70 w-[30rem] z-10 p-12 rounded">
-      <p className="text-3xl font-bold mb-6">Sign Up</p>
+    <div className="bg-black/70 w-[30rem] z-10 p-14 rounded">
+      <p className="text-3xl font-medium mb-6">Sign Up</p>
       <div className="flex flex-col">
         <form className="flex flex-col gap-5">
           <input
             type="email"
             placeholder="Email"
-            className="h-14 rounded text-white bg-gray-100"
+            className="h-12 rounded text-white pl-4 bg-neutral-600"
+
           />
           <input
             type="password"
             placeholder="Password"
-            className="h-14 rounded text-white bg-gray-100"
+            className="h-12 rounded text-white pl-4 bg-neutral-600"
+
           />
           <input
             type="password"
             placeholder="Repeat your password"
-            className="h-14 rounded text-white bg-gray-100"
+            className="h-12 rounded text-white pl-4 bg-neutral-600"
+
           />
           <button
             type="submit"
-            className="bg-red-100 h-14 rounded text-xl"
+            className="bg-red-700 h-12 rounded text-xl"
           >
             Sign Up
           </button>
         </form>
         <div className="h-px w-[90%] bg-white/30 mt-6 mb-5 m-auto"></div>
 
-        <div className="flex gap-2 text-lg justify-center mt-2">
+        <div className="flex gap-2  justify-center mt-2">
           <p className="text-white/30">Do you already have an account?</p>
           <p
-            className="text-red-200 font-bold cursor-pointer hover:underline"
+            className="cursor-pointer hover:underline"
             onClick={() => router.push("/login")}
           >
             Sign In
