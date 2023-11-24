79a36d043bdbc92252052762643a6134352bc9f4
RuchiRay <ruchiray18@gmail.com>
2023-11-24 23:24:06 +0530

    added changes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,29 @@
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import React, { useState, useEffect } from 'react'
 
 export const Navbar = () => {
-    const [bgBlack, setBgBlack] = useState(false)
-    const pathname = usePathname()
-    console.log(pathname,'pathname');
-    if(pathname==='/')
+  const [bgBlack, setBgBlack] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+  const router = useRouter()
+  const handleLogOut = () => {
+    router.push('/login')
+  };
+  const pathname = usePathname()
+  console.log(pathname, 'pathname');
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 50)
+        setBgBlack(true)
+      else
+        setBgBlack(false)
+    }
+    window.addEventListener('scroll', handler)
+    return () => window.removeEventListener("scroll", handler);
+  }, [])
+  if (pathname === '/')
     return
   return (
     <div className={`${bgBlack && 'bg-black'} fade-top transition-colors ease-linear items-center  flex fixed w-full justify-between py-3  px-12 left-0 top-0 z-[1]`}>
@@ -24,7 +39,7 @@ export const Navbar = () => {
         <div className="text-white flex gap-8 text-sm ml-12">
           <Link
             href="/"
-            // className={({ isActive }) => (isActive ? 'font-bold' : "")}
+          // className={({ isActive }) => (isActive ? 'font-bold' : "")}
           >
             Home
           </Link>
@@ -41,18 +56,17 @@ export const Navbar = () => {
           width={30}
           height={30}
           className='rounded'
-        //   onClick={() => setShowMenu(!showMenu)}
+          onClick={() => setShowMenu(!showMenu)}
         />
-        {/* <div
-          className={`${
-            showMenu ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-          } flex border absolute w-28 p-2 flex-col bg-black transition duration-300 ease-in-out right-0 top-16 border-white/50`}
+        <div
+          className={`${showMenu ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+            } flex border absolute w-28 p-2 flex-col bg-black transition duration-300 ease-in-out right-0 top-16 border-white/50`}
         >
           <button className="text-left">My Profile</button>
           <button className="text-left" onClick={handleLogOut}>
             Log Out
           </button>
-        </div> */}
+        </div>
       </div>
     </div>
   )
